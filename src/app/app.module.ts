10212616bbf9af905e3efa8c20d482510711ce52
Aguarico <126192913+Aguarico@users.noreply.github.com>
2023-02-23 20:57:57 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-//import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 //import { MatFormFieldModule } from "@angular/material/form-field";
 //import { MatInputModule } from "@angular/material/input";
 //import { MatButtonModule } from "@angular/material/button";
 //import { MatIconModule } from "@angular/material/icon";
 //import { MatTooltipModule } from "@angular/material/tooltip";
-import { AppComponent } from "./app.component";
-import { MgPreviewModule } from "./mg-preview/mg-preview.module";
-import { BlobPipe } from './blob.pipe';
+
+
+//import { AppComponent } from "./app.component";
+//import { MgPreviewModule } from "./mg-preview/mg-preview.module";
+//import { BlobPipe } from './blob.pipe';
 
 @NgModule({
   imports: [
@@ -18,16 +20,16 @@ import { BlobPipe } from './blob.pipe';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    FlexLayoutModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatTooltipModule,
-    MgPreviewModule
+    //FlexLayoutModule,
+    //MatFormFieldModule,
+    //MatButtonModule,
+    //MatIconModule,
+    //MatInputModule,
+    //MatTooltipModule,
+    //MgPreviewModule
   ],
 
-  declarations: [AppComponent, BlobPipe],
-  bootstrap: [AppComponent]
+  //declarations: [AppComponent, BlobPipe],
+  //bootstrap: [AppComponent]
 })
 export class AppModule {}
