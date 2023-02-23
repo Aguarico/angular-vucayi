7d81e722f8a91245941984c8faa95044215fc18d
Aguarico <126192913+Aguarico@users.noreply.github.com>
2023-02-23 20:44:21 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { FlexLayoutModule } from "@angular/flex-layout";
+//import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 //import { MatFormFieldModule } from "@angular/material/form-field";
 //import { MatInputModule } from "@angular/material/input";
 //import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTooltipModule } from "@angular/material/tooltip";
+//import { MatIconModule } from "@angular/material/icon";
+//import { MatTooltipModule } from "@angular/material/tooltip";
 import { AppComponent } from "./app.component";
 import { MgPreviewModule } from "./mg-preview/mg-preview.module";
 import { BlobPipe } from './blob.pipe';
